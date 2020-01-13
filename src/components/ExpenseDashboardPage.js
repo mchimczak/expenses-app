503da47cdbb55a4def575909c7daf32d805829dc
Michał <michal.chimczak@gmail.com>
2020-01-13 17:27:37 +0100

    Add ExpensesSummary feature

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
-import ExpenseListFilters from './ExpenseListFilters'
+import ExpenseListFilters from './ExpenseListFilters';
+import ExpensesSummary from './ExpensesSummary'
 
 
 
 const ExpenseDashboardPage = () => (
     <div>
         <p>New app from Dash component</p>
+        <ExpensesSummary/>
         <ExpenseListFilters/>
         <ExpenseList/>
     </div>
